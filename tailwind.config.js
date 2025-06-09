2916a3e152083e66c1d859690242a842e74b3e8b
yanadoesntcode <yanadspasova@gmail.com>
2025-06-09 15:38:45 +0200

    trying to incorporate a font

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          blackmango: ['"Black Mango"', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
